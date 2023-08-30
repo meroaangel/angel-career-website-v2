97f4269974a157c6d03e0d36904612e6c8dfbf92
meroaangel <25314010+meroaangel@users.noreply.github.com>
2023-08-30 12:22:13 +0000

    company_name added in app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ JOBS = [
 
 @app.route("/")
 def hello_angel():
-  return render_template('home.html', jobs=JOBS)
+  return render_template('home.html', jobs=JOBS, company_name='Angel')
 
 @app.route("/api/jobs")
 def list_jobs():
